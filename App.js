dc427d8a01ddece47a6f2b6123bccd6711df673b
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2022-04-11 22:53:03 +0100

    flexbox demo

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {{
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <View style = {{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100
+      }}/>
+      <View style = {{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100
+      }}/>
+      <View style = {{
+        backgroundColor: 'green',
+        width: 100,
+        height: 100
+      }}/>
     </View>
   );
 }
